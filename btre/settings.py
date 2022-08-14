e6e93044bb1aa66a3be11366abe457395c226a22
Jocelyn Rodriguez <jossyrodriguez@gmail.com>
2022-08-14 11:55:49 -0700

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -151,3 +151,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jossyrodriguez@gmail.com'
 EMAIL_HOST_PASSWORD = 'JABBAjuice14!!'
 EMAIL_USE_TLS = True 
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass 
